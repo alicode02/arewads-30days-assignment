79e710da858d11a5acb136f1202636a95599954a
alicode02 <aliybaba3718gmail.com>
2023-12-13 23:37:42 +0100

    my day2 assignment

---
+++ Day 2/variable.py
@@ -0,0 +1,17 @@
+firstname = Aliyu
+print(firstname)
+last_name = Saleh
+print(last_name)
+fullname = Ali Saleh
+print(fullname)
+country = Nigeria
+print(country)
+city = Abuja
+print(city)
+age = 150
+print(age)
+year = 2023
+print(year)
+is_married = True
+print(is_married)
+first_name_last_name_age_country = Ali Saleh 150 Nigeria
