2cde46b2058c57188d721b519fd7da53e61eaf0b
alicode02 <aliybaba3718gmail.com>
2023-12-16 23:11:55 +0100

    my day 4 assignment

---
+++ Day 4/string.py
@@ -0,0 +1,19 @@
+a = 'thirty'
+b = 'days'
+c = 'of' 
+d = 'python'
+challenge = a + b + c + d
+print(challenge)
+x = 'coding'
+y = 'for'
+z = 'all'
+result = x + y + z
+print(result)
+company = 'coding for all'
+print(company)
+print (len(company))
+print(company.isupper())
+print(company.capitalize())
+print(company.swapcase())
+print(company.upper())
+print(company.lower())
