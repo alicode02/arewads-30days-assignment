5d9f331193285e1790c924f9f1bd5918e3865e8c
alicode02 <aliybaba3718gmail.com>
2023-12-18 20:30:40 +0100

    My day6 assignment

---
+++ Day 5/list.py
@@ -0,0 +1,29 @@
+list = []
+print(list)
+fruits = ['mango','orange','pineapple','guava','apple']
+print(fruits)
+print('number of fruits:', len(fruits))
+mixed_data= ['aliyu','250','183cm','single','Nigeria']
+print(mixed_data)
+it_companies = ['facebook','google','microsoft','apple','IBM','oracle','amazon']
+print(it_companies)
+print(len(it_companies))
+first_company = it_companies[0]
+print(first_company)
+middle_company = it_companies[3]
+print(middle_company)
+last_company = it_companies[6]
+print(last_company)
+last_item = len(it_companies) - 1
+print(last_item)
+last_company = it_companies[last_item]
+print(last_company)
+it_companies.append('mckintosh')
+print(it_companies)
+it_companies.insert(3,'AT&T')
+print(it_companies)
+first_three = it_companies[0:4]
+print(first_three)
+last_three = it_companies[6:]
+print(last_three)
+
