191d75bfc2128edb0eed6d8a0b588dea5de39c4c
alicode02 <aliybaba3718gmail.com>
2023-12-18 23:06:35 +0100

    My Day 6 Assignment

---
+++ Day 6/turple.py
@@ -0,0 +1,16 @@
+tpl = ()
+print(tpl)
+sisters = ('aisha','amina','zainab','maryam')
+print(sisters)
+brothers = ('abdul','kamal','yusuf','ali')
+print(brothers)
+brothers_sisters = sisters + brothers
+print(brothers_sisters)
+len(brothers_sisters)
+print(len(brothers_sisters))
+siblings = list(brothers_sisters)
+print(siblings)
+siblings[0] = 'father'
+print(siblings)
+siblings[1] = 'mother'
+print(siblings)
