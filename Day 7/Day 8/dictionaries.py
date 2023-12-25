3230067130ab77b083796fc0a2c8f38ea1423b22
alicode02 <aliybaba3718gmail.com>
2023-12-25 23:17:20 +0100

    My Day 8 assignment

---
+++ Day 7/Day 8/dictionaries.py
@@ -0,0 +1,11 @@
+dog = {}
+print(dog)
+dog = {'name:chedder', 'color:white','legs:4', 'age:2'}
+print(dog)
+student = {'first_name:aliyu','last_name:haidar','gender:male','age:250','marital_status:single','country:nigeria','city:abuja','address:tokyo'}
+print(student)
+value = student.values()
+keys = student.keys()
+print(keys)
+print(student.item())
+
