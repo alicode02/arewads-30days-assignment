372ca91a42e29195da31473d156fea18996939d7
alicode02 <aliybaba3718gmail.com>
2024-01-02 21:45:03 +0100

    My Day 9 assignment

---
+++ Day 7/Day 9/conditionals.py
@@ -0,0 +1,10 @@
+age = 30
+if age > 15:
+    print('you are old enough to drive')
+elif age == 15:
+      print('you need 3 more years to learn to drive')
+my_age = 30
+if age == 25:
+     print('we are of same age')
+else:
+     print('you are 5 years older than me')
