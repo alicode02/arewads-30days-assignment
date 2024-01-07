44eaa827fa43bba745e80eb639d3c7d0e610830b
alicode02 <aliybaba3718gmail.com>
2024-01-07 17:23:09 +0100

    This is my Day 11 assignment

---
+++ Day 7/Day 11/functions.py
@@ -0,0 +1,11 @@
+def add_two_numbers ():
+    num_one = 5
+    num_two = 10
+    total = num_one + num_two
+    print(total)
+add_two_numbers()
+def area_of_circle (r):
+    PI = 3.14
+    area = PI * r ** 2
+    retun area
+print(area_of_circle(5))
